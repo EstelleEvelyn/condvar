e78367ff437a612657df3967c78bec7fe904581f
EstelleEvelyn <estelle.bayer@gmail.com>
2017-10-11 14:38:57 -0500

    e

--- kidboat.c
+++ kidboat.c
@@ -54,6 +54,7 @@ void* childThread(void* args) {
       }
       leaveBoat(KID, MOLO);
       kidsOnBoard--;
+      pthread_cond_signal(&kidsBoardOahu);
       pthread_cond_signal(&allDone);
       pthread_mutex_unlock(&lock);
     } else {
