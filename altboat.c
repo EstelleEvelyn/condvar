83acb807d5729a35f8ed28589dafcf86682485c7
EstelleEvelyn <estelle.bayer@gmail.com>
2017-10-11 21:24:19 -0500

    e

--- altboat.c
+++ altboat.c
@@ -106,6 +106,7 @@ void* childThread(void* args) {
         pthread_cond_signal(&kidsBoardOahu);
       } else {
         leaveBoat(KID, MOLO);
+        fflush(stdout);
         kidsOnBoard--;
         // pthread_cond_signal(&allDone);
       }
